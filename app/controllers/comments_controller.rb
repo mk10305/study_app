d72621c12be86c68b7dd7a8043ca710a86ec7991
Madk1030 <madk1030@gmail.com>
2014-10-15 12:37:08 -0400

    Updates

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+
+
+def create
+  @post = Post.find(params[:post_id])
+
+  @comment = Comment.new(params.require(:comment).permit(:body))
+  @comment.post = @post
+  @comment.creator = User.first
+  
+
+  if @comment.save 
+
+    flash[:notice] = "Your comment was added"
+    redirect_to post_path(@post)
+
+  else
+    render 'posts/show'
+  end
+
+
+end
+
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ActiveRecord::Base
  has_many :post_categories
  has_many :posts, through: :post_categories
 
+
+
+
+ validates :name, presence: true
+
 end
