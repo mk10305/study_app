262a0f013084d9fda91a182a1d009a54ad37daeb
Madk1030 <madk1030@gmail.com>
2014-12-04 12:52:26 -0500

    updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:index, :show]
-before_action :authorize, only: [:edit,:update,:destroy]
+  before_action :authorize, only: [:edit,:update,:destroy]
 
   def index
     @posts = Post.all.sort_by{|x| x.total_votes}.reverse
@@ -32,7 +32,7 @@ before_action :authorize, only: [:edit,:update,:destroy]
   end
 
   def edit
-  
+
 
   end
 
@@ -48,22 +48,27 @@ before_action :authorize, only: [:edit,:update,:destroy]
 
   end
 
-def vote
- vote =  Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+  def vote
+   @vote =  Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
 
-if vote.valid? 
-  flash[:notice] = 'Your vote was counted.'
-else
-  flash[:error] = 'You can only vote on a post once'
+   respond_to do |format|
+    format.html do
+
+      if @vote.valid? 
+        flash[:notice] = 'Your vote was counted.'
+      else
+        flash[:error] = 'You can only vote on a post once'
+      end
+      redirect_to :back
+    end
+
+    format.js 
+
+  end
+
 end
 
-  redirect_to :back
-end
 
-
-
-
-  
 private
 def authorize
   unless @post.creator == current_user
@@ -71,11 +76,11 @@ def authorize
     redirect_to root_path
     return false
   end
-  end
+end
 
 def post_params
   params.require(:post).permit(:title, :url, :description, category_ids: [])
-  end
+end
 
 def set_post
   @post = Post.find(params[:id])
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,8 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
+  before_save :generate_slug
+
 
   def total_votes
     self.up_votes - self.down_votes
@@ -26,6 +28,13 @@ class Post < ActiveRecord::Base
     self.votes.where(vote: false).size
   end
 
+
+
+
+
+  def generate_slug
+    self.slug = self.title.gsub(" ", "-").downcase
+  end
 
 
 end
