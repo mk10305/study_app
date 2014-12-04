ea1f2c0cb5fcac74da8bbe95a26690bd0b842f29
Madk1030 <madk1030@gmail.com>
2014-12-04 09:41:08 -0500

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,21 @@ PostitTemplate::Application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   resources :posts, except: [:destroy] do
-      resources :comments, only: [:create]
+
+    member do 
+      post 'vote'
+    end
+
+
+
+
+    resources :comments, only: [:create] do
+      member do 
+        post 'vote'
+      end
+    end
+
+
   end
 
   resources :categories, only: [:new, :create, :show]
@@ -15,3 +29,4 @@ PostitTemplate::Application.routes.draw do
   resources :users, only: [:show, :create, :edit, :update]
 
 end
+
